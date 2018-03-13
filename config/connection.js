c4a29e4a1ab07584aeefd1aa29ff97eb5b5c6737
Julie Groth <groth.julie@gmail.com>
2018-03-12 19:35:48 -0700

    updated connection to db to include JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,20 @@
 require('dotenv').config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+  });
+}
+
+
 
 // make connection
 connection.connect(function(err) {
