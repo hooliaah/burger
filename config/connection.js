85a00e03eaa7173aaa625a1accf81020211a757e
Julie Groth <groth.julie@gmail.com>
2018-03-12 19:43:40 -0700

    updated jaws url

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 var mysql = require("mysql");
 
 var connection;
-if (process.env.JAWSDB) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
